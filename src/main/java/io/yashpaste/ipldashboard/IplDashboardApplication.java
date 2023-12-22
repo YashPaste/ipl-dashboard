d7df2b633ea79b56c53269ee23ffadf4baeab3f2
yashpaste <yashp.nitt@gmail.com>
2023-12-22 21:48:39 +0530

    added execution changes

--- src/main/java/io/yashpaste/ipldashboard/IplDashboardApplication.java
+++ src/main/java/io/yashpaste/ipldashboard/IplDashboardApplication.java
@@ -1,9 +1,13 @@
 package io.yashpaste.ipldashboard;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableBatchProcessing
+@ComponentScan(basePackages = "io.yashpaste.ipldashboard")
 public class IplDashboardApplication {
 
 	public static void main(String[] args) {
